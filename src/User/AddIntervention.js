a27d85ba11b0b2cfb14705b5d4452a7e1bb72531
Joseph_Thairu <joejoement@gmail.com>
2024-08-07 22:40:34 +0300

    geolocation selector added for addredflags and addintervention

--- src/User/AddIntervention.js
+++ src/User/AddIntervention.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import './UpdateForm.css';
 
 export default function AddIntervention() {
+    const [city, setCity] = useState(''); 
+    const [geolocation, setGeolocation] = useState('');  
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -24,6 +28,30 @@ export default function AddIntervention() {
         }
     };
 
+    const handleGeocode = () => {
+        axios.get('https://nominatim.openstreetmap.org/search', {
+          params: {
+            q: city,
+            format: 'json'
+          }
+        })
+        .then(response => {
+          if (response.data.length > 0) {
+            const { lat, lon } = response.data[0];
+            setGeolocation(`${lat}, ${lon}`);
+          } else {
+            alert('Location not found');
+          }
+        })
+        .catch(error => {
+          console.error('Geocoding error:', error);
+        });
+      };
+
+    const handleCityChange = (e) => {
+        setCity(e.target.value);
+      };
+
     return (
         <div className="update-form">
             <h2>Report an Intervention</h2>
@@ -40,12 +68,28 @@ export default function AddIntervention() {
                         <textarea name="description" />
                     </label>
                 </div>
-                <div className="form-group">
-                    <label>
-                        Geolocation:
-                        <input type="text" name="geolocation" />
-                    </label>
+                <div className='form-group'>
+                 <label htmlFor="city">City</label>
+                    <input 
+                        type='text' 
+                        id='city' 
+                        name='city' 
+                        value={city} 
+                        onChange={handleCityChange}
+                    />
+                <div className="form-buttons" ><button type="button"  onClick={handleGeocode}>Get Coordinates</button></div>
+          </div>
+          <div className="form-group">
+                    <label htmlFor="geolocation">Geolocation (Lat, Long)</label>
+                            <input 
+                                type='text' 
+                                id='geolocation' 
+                                name='geolocation' 
+                                value={geolocation} 
+                                readOnly
+                            />
                 </div>
+                
                 <div className="form-group file-input-wrapper">
                     <label className="file-input-label">
                         Choose Image
